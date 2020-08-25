84719644f9f93d539ca0fead33fe371276971e91
Ruslan <ruslanehg@gmail.com>
2020-08-26 00:43:43 +0600

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,24 +17,44 @@ $(document).ready(function(){
         relations = [];
         figureWidth = 50;
         selectedNode = false;
+        isMouseDown = false;
 
         constructor() {
             //todo: Сделать проверку на присутствие связей
             this.relations = [];
 
             if(this.relations.length == 0){
-                this.addFigure(canvas.width / 2 - this.figureWidth, canvas.height / 2 - this.figureWidth, this.figureWidth);
+                this.addNode(canvas.width / 2 - this.figureWidth, canvas.height / 2 - this.figureWidth);
                 this.draw();
             }
         }
 
-        addFigure = (x, y, width = this.figureWidth) => {
+        addNode = (x, y, node = false, width = this.figureWidth) => {
             this.nodes.push(new Node(x, y, width));
+
+            if(node) {
+                let lastNode = this.getLastNode();
+                let relation = new Relation(node, lastNode);
+
+                this.relations.push(relation);
+
+                lastNode.relations.push(relation);
+            }
+        }
+
+        getLastNode = () => {
+            return this.nodes[this.nodes.length - 1];
         }
 
         draw = () => {
             this.nodes.map((node) => {
                 node.draw();
+                console.log(node.relations);
+                if(node.relations.length != 0){
+                    node.relations.map((relation) => {
+                        relation.draw();
+                    });
+                }
             })
         }
 
@@ -73,6 +93,7 @@ $(document).ready(function(){
         ];
         selectedSection;
         figure;
+        sidesCoords = {};
         constructor(x, y, width) {
             this.coords = {
                 x: x,
@@ -83,6 +104,38 @@ $(document).ready(function(){
 
             this.sides.forEach((side) => {
                 this.sections.push(new Section(this, side));
+            });
+
+            this.defineSideCoords();
+        }
+
+        defineSideCoords = () => {
+            let x = 0, y = 0;
+            this.sides.map((side) => {
+                x = 0;
+                y = 0;
+                switch (side) {
+                    case 'top':
+                        x = this.coords.x + this.width / 2;
+                        y = this.coords.y;
+                        this.sidesCoords.top = {x: x, y: y};
+                        break;
+                    case 'right':
+                        x = this.coords.x + this.width;
+                        y = this.coords.y + this.width / 2;
+                        this.sidesCoords.right = {x: x, y: y};
+                        break;
+                    case 'bot':
+                        x = this.coords.x + this.width / 2;
+                        y = this.coords.y + this.width;
+                        this.sidesCoords.bot = {x: x, y: y};
+                        break;
+                    case 'left':
+                        x = this.coords.x;
+                        y = this.coords.y + this.width / 2;
+                        this.sidesCoords.left = {x: x, y: y};
+                        break;
+                }
             });
         }
 
@@ -103,14 +156,26 @@ $(document).ready(function(){
             });
         }
 
-        move = (x, y) => {
-            this.coords = {
-                x: x - this.width / 2,
-                y: y - this.width / 2
+        move = (x, y, drag = false) => {
+            if(drag){
+                this.coords = {
+                    x: x,
+                    y: y
+                }
+                this.sections.forEach((section) => {
+                    section.move();
+                });
+                this.defineSideCoords();
+            } else {
+                this.coords = {
+                    x: x - this.width / 2,
+                    y: y - this.width / 2
+                }
+                this.sections.forEach((section) => {
+                    section.move();
+                });
+                this.defineSideCoords();
             }
-            this.sections.forEach((section) => {
-                section.move();
-            });
         }
 
         check = (x, y) => {
@@ -207,6 +272,57 @@ $(document).ready(function(){
         }
     }
 
+    class Relation{
+        section = {};
+        parent = {};
+        child = {};
+        constructor(parent, child) {
+            this.child = child;
+            this.section = parent;
+            this.parent = parent.parent;
+        }
+
+        draw = () => {
+            ctx.beginPath();
+
+            let fromX, fromY, toX, toY;
+
+            switch (this.section.type) {
+                case 'top':
+                    fromX = this.parent.sidesCoords.top.x;
+                    fromY = this.parent.sidesCoords.top.y;
+                    toX = this.child.sidesCoords.bot.x;
+                    toY = this.child.sidesCoords.bot.y;
+                    break;
+                case 'right':
+                    fromX = this.parent.sidesCoords.right.x;
+                    fromY = this.parent.sidesCoords.right.y;
+                    toX = this.child.sidesCoords.left.x;
+                    toY = this.child.sidesCoords.left.y;
+                    break;
+                case 'bot':
+                    fromX = this.parent.sidesCoords.bot.x;
+                    fromY = this.parent.sidesCoords.bot.y;
+                    toX = this.child.sidesCoords.top.x;
+                    toY = this.child.sidesCoords.top.y;
+                    break;
+                case 'left':
+                    fromX = this.parent.sidesCoords.left.x;
+                    fromY = this.parent.sidesCoords.left.y;
+                    toX = this.child.sidesCoords.right.x;
+                    toY = this.child.sidesCoords.right.y;
+                    break;
+            }
+            console.log(fromX);
+            console.log(toX);
+            ctx.moveTo(fromX, fromY);
+            ctx.lineTo(toX, toY);
+            ctx.stroke();
+
+            ctx.closePath();
+        }
+    }
+
     /*let fig = new Node(100, 100, 50)
     fig.draw();
 
@@ -231,49 +347,89 @@ $(document).ready(function(){
     canvas.addEventListener('mousedown', (e) => {
         mouse.x = e.clientX;
         mouse.y = e.clientY;
+        board.isMouseDown = true;
+
 
         //board.clear();
         if(board.check(mouse.x, mouse.y)){
             if(board.selectedNode.constructor.name == 'Section'){
                 console.log('section');
-                let x = board.selectedNode.coords.x;
-                let y = board.selectedNode.coords.y;
+                let x = board.selectedNode.parent.coords.x;
+                let y = board.selectedNode.parent.coords.y;
                 switch(board.selectedNode.type) {
                     case 'top':
-                        board.addFigure(x, y - board.figureWidth * 2);
+                        board.addNode(x, y - board.figureWidth * 2, board.selectedNode);
                         break;
                     case 'right':
-                        board.addFigure(x + board.figureWidth * 2, y);
+                        board.addNode(x + board.figureWidth * 2, y, board.selectedNode);
                         break;
                     case 'bot':
-                        board.addFigure(x, y + board.figureWidth * 2);
+                        board.addNode(x, y + board.figureWidth * 2, board.selectedNode);
                         break;
                     case 'left':
-                        board.addFigure(x - board.figureWidth * 2, y);
+                        board.addNode(x - board.figureWidth * 2, y, board.selectedNode);
                         break;
                 }
                 board.draw();
-            } else if(e.shiftKey && board.check(mouse.x, mouse.y)){
-
             }
         }
-
     });
 
     canvas.addEventListener('mouseup', (e) => {
         board.selectedNode = false;
+        board.isMouseDown = false;
     });
 
 
-    canvas.addEventListener('mousemove', (e) => {
-        if(e.shiftKey && board.selectedNode){
-            mouse.x = e.clientX;
+    canvas.addEventListener('mousemove', function (e){
+        console.log('node:', board.selectedNode);
+        let page = {
+            x: e.pageX,
+            y: e.pageY
+        }
+        /*let x = e.pageX,
+            y = e.pageY;*/
+        if(e.shiftKey && board.selectedNode && board.isMouseDown){
+            /*mouse.x = e.clientX;
             mouse.y = e.clientY;
 
-            //board.clear();
-            //board.check(mouse.x, mouse.y)
             board.clear();
             board.selectedNode.move(mouse.x, mouse.y);
+            board.draw();*/
+
+            board.clear();
+
+            let offset = {
+                x: page.x - mouse.x,
+                y: page.y - mouse.y
+            };
+
+            board.selectedNode.coords.x += offset.x;
+            board.selectedNode.coords.y += offset.y;
+            board.selectedNode.move(board.selectedNode.coords.x, board.selectedNode.coords.y, true);
+
+            mouse.x = page.x;
+            mouse.y = page.y;
+
+            board.draw();
+        }
+        if(e.shiftKey && !board.selectedNode && board.isMouseDown) {
+            board.clear();
+
+            let offset = {
+                x:page.x-mouse.x,
+                y:page.y-mouse.y
+            };
+
+            board.nodes.map((node) => {
+                node.coords.x += offset.x;
+                node.coords.y += offset.y;
+                node.move(node.coords.x, node.coords.y, true);
+            });
+
+            mouse.x = page.x;
+            mouse.y = page.y;
+
             board.draw();
         }
     });
