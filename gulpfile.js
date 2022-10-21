3bbc89f55a09d373e17d71e15fcf8f9ee272e3ea
Ruslan S <ruslanehg@gmail.com>
2022-10-21 15:21:38 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,7 @@ gulp.task('watch', function() {
         notify: false,
     });
 
-    // gulp.watch('assets/css/*.css', browserSync.reload);
-    // gulp.watch('assets/js/*.js', browserSync.reload);
     gulp.watch('assets/css/*.css').on('change', browserSync.reload);
     gulp.watch('assets/js/*.js').on('change', browserSync.reload);
-    //gulp.watch(['assets/libs/**/*.js', 'app/js/common.js'], ['js']);
-    //gulp.watch('./*.html', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
 });
